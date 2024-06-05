7513f1f47422695e4ac0bc9859a92e55b487149e
Tanmay Sharma <sharmatanmay617@gmail.com>
2024-06-05 15:55:05 +0300

    Port Changed

--- main.py
+++ main.py
@@ -35,4 +35,4 @@ def health():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=3000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
